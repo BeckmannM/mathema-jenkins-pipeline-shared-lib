9c06c667fd977f33b098ec21bbdc4908a9631e8b
Manuel <Manuel@Manuel-PC>
2017-04-27 18:51:03 +0200

    Aufräumen

--- vars/myPostActions.groovy
+++ vars/myPostActions.groovy
@@ -3,5 +3,5 @@ import com.godyo.mathema.jenkins.shared.Logger
 def call() {
 
 	def logger = new Logger(steps)
-	logger.log("Nun werden die post build-Aktionen ausgefuehrt")
+	logger.log("Wird sind fertig :-)")
 }
